ee36f9df08e65097b51abd41bf30ffde6b275b2e
inglepriyanshu <inglepriyanshu911@gmail.com>
2024-02-14 08:50:16 +0530

    Solved JWT

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,5 +1,9 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const zod = require("zod");
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6)
 
 
 /**
@@ -15,6 +19,19 @@ const jwtPassword = 'secret';
  */
 function signJwt(username, password) {
     // Your code here
+    usernameResponse = emailSchema.safeParse(username);
+    passwordResponse = passwordSchema.safeParse(password);
+    
+    if(!usernameResponse.success || !passwordResponse.success)
+    {
+        return null;
+    }
+    const obj = {
+        username : username,
+        password : password
+    }
+    const key = jwt.sign(obj,jwtPassword);
+    return key;
 }
 
 /**
@@ -27,6 +44,12 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    try {
+        const tokenVerify = jwt.verify(token, jwtPassword);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
 /**
@@ -38,6 +61,12 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const decoded = jwt.decode(token);
+    if (decoded) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
